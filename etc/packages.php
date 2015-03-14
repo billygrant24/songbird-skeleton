245ec68b8a40c081c36edf75db37a36a7e0328d2
William Grant-Buckley <william@codist.uk>
2015-03-14 03:21:23 +0000

    Update packages.php

--- etc/packages.php
+++ etc/packages.php
@@ -8,13 +8,8 @@ return [
 
     'packages' => [
 
-        /*
-         * This provides our templating engine. If you remove this, you must implement another engine following the
-         * Template contract.
-         */
-
-        'Songbird\Package\Plates\PlatesServiceProvider',
-        'Songbird\Package\CommonMark\MarkdownServiceProvider',
+        'Songbird\Package\CommonMark\CommonMarkProvider',
+        'Songbird\Package\Plates\PlatesProvider',
 
     ],
 
